476b17a830e06e968dd4529e2f56c1d4a42e0c2d
biibo <biibolot23@gmail.com>
2023-04-23 11:29:29 +0300

    vd

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+      Triangle triangle = new Triangle();
+
+     triangle.A = 16;
+     triangle.B = 16;
+     triangle.C = 16;
+
+     triangle.area();
+    }
+
+}
---
+++ src/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle {
+
+    int A;
+    int B;
+    int C;
+
+     void area(){
+         System.out.println((A+B+C)/2);
+
+    }
+}
